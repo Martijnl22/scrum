237814f218d643fe26452f6d5eaa666c41a22233
Martijnl22 <0251673@student.rocvantwente.nl>
2018-11-15 10:59:31 +0100

    Toegevoegd: 1.1
    Opdracht 1.1 toegevoegd aan map( Ontwikkelingsomgeving)

---
+++ Back-end/S1-BE-M00-Ontwikkelingsomgeving/Opdracht 1.1/Opdracht 1.1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Johan Martijn Legtenberg");
+            Console.WriteLine("1e Esweg 12");
+            Console.WriteLine("7642BK Wierden");
+            Console.WriteLine("22 Jaar");
+            Console.ReadLine();
+        }
+    }
+}
